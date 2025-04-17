426bbb828800a7517d14bdcf877875db732678be
Zyad Yakan <18p9538@eng.asu.edu.eg>
2025-04-17 20:22:59 +0200

    Implement AI interviewer UI
    Implemented AI interview page UI and integrated it with the interviews route

---
+++ app/(root)/interview/page.tsx
@@ -0,0 +1,21 @@
+import Agent from '@/components/Agent'
+import React from 'react'
+
+const page = () => {
+  return (
+      <>
+        <h3>
+            Interview Generation Process  
+        </h3> 
+        <Agent
+            userName="You"
+            userId='user1'
+            type="generate"
+          
+        />
+
+    </>
+  )
+}
+
+export default page
